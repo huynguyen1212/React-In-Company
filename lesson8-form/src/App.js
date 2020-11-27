167b1ec3725dca5c173a3119d74a91597d4457a6
HuyNguyen <huynguyen1212.com>
2020-11-28 01:04:00 +0700

    done

--- lesson8-form/src/App.js
+++ lesson8-form/src/App.js
@@ -5,17 +5,27 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
+      txtUsername: "",
+      txtPassword: "",
+      txtDesc: "",
+      sltGender: 0,
+      rdLang: "vi",
+      chkbStatus: false,
     };
     this.onHandleChange = this.onHandleChange.bind(this);
+    this.onHandleSubmit = this.onHandleSubmit.bind(this);
   }
   onHandleChange(event) {
+    var target = event.target;
+    var name = target.name;
+    var value = target.type === "checkbox" ? target.checked : target.value;
     this.setState({
-      username: event.target.value,
+      [name]: value,
     });
   }
   onHandleSubmit(event) {
     event.preventDefault();
+    console.log(this.state);
   }
   render() {
     return (
@@ -33,9 +43,78 @@ class App extends Component {
                     <input
                       type="text"
                       className="form-control"
-                      name="txtName"
+                      name="txtUsername"
+                      onChange={this.onHandleChange}
+                      value={this.state.txtUsername}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Password : </label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="txtPassword"
+                      value={this.state.txtPassword}
                       onChange={this.onHandleChange}
                     />
+                  </div>
+                  <div className="form-group">
+                    <label>Mô tả : </label>
+                    <textarea
+                      name="txtDesc"
+                      className="form-control"
+                      rows="3"
+                      value={this.state.txtDesc}
+                      onChange={this.onHandleChange}
+                    ></textarea>
+                  </div>
+                  <label>Giới tính : </label>
+                  <select
+                    name="sltGender"
+                    className="form-control"
+                    value={this.state.sltGender}
+                    onChange={this.onHandleChange}
+                  >
+                    <option value={0}>Nữ</option>
+                    <option value={1}>Nam</option>
+                  </select>
+                  <br />
+                  <label>Ngôn ngữ : </label>
+                  <div className="radio">
+                    <label>
+                      <input
+                        type="radio"
+                        name="rdLang"
+                        value="en"
+                        onChange={this.onHandleChange}
+                        checked={this.state.rdLang === "en"}
+                      />
+                      Tiếng Anh
+                    </label>
+                    <br />
+                    <label>
+                      <input
+                        type="radio"
+                        name="rdLang"
+                        value="vi"
+                        onChange={this.onHandleChange}
+                        checked={this.state.rdLang === "vi"}
+                      />
+                      Tiếng Việt
+                    </label>
+                  </div>
+                  <br />
+                  <div class="checkbox">
+                    <label>
+                      <input
+                        type="checkbox"
+                        value={true}
+                        name="chkbStatus"
+                        onChange={this.onHandleChange}
+                        checked={this.state.chkbStatus === true}
+                      />
+                      Trạng Thái : 
+                    </label>
                   </div>
                   <button type="submit" className="btn btn-primary">
                     Lưu lại
