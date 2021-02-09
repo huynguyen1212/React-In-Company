44af814484cbcb1e99c8892b53d0593d8125a59d
huynguyen1212 <huyro1212000@gmail.com>
2021-02-09 23:41:11 +0700

    API new project 4

--- lesson16-call-api/src/App.js
+++ lesson16-call-api/src/App.js
@@ -1,24 +1,43 @@
 import React from "react";
+import { Route, Router, Switch } from "react-router-dom";
 import "./App.css";
 import Menu from "./components/Menu/Menu"
 import ProductList from "./components/ProductList/ProductList"
+import routes from "./routes";
 
-function App() {
+function App(props) {
+  const showContentMenus = (routes) => {
+    let result = null;
+    if (routes.length > 0) {
+      result = routes.map((route, index) => {
+        return (<Route
+          key={index}
+          path={route.path}
+          exact={route.exact}
+          component={route.main}
+        />);
+      });
+    }
+    return <Switch>{result}</Switch>
+  }
+
   return (
-    <div>
-      <Menu />
-
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+    <Router>
+      <div>
+        <Menu />
+        <div className="container">
+          <div className="row">
+            {/* <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <button type="button" className="btn btn-info mb-10">
               Thêm sản phẩm
             </button>
             <ProductList />
+          </div> */}
+            {showContentMenus(routes)}
           </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
