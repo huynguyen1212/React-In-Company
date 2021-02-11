d55d708dc6a7241302c2a9d55d83d03762a31f92
huynguyen1212 <huyro1212000@gmail.com>
2021-02-11 17:56:43 +0700

    API new project 6

--- lesson16-call-api/src/components/Menu/Menu.js
+++ lesson16-call-api/src/components/Menu/Menu.js
@@ -1,16 +1,60 @@
 import React from "react";
+import { Link, Route } from "react-router-dom";
 
 function Menu() {
+  const menus = [
+    {
+      name: "Trang chủ",
+      to: "/",
+      exact: true,
+    },
+    {
+      name: "Quản lí sản phẩm",
+      to: "/product-list",
+      exact: false,
+    }
+  ];
+
+  const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
+    return (
+      <Route
+        path={to}
+        exact={activeOnlyWhenExact}
+        children={({ match }) => {
+          var active = match ? "active" : "";
+          return (
+            <li className={active}>
+              <Link to={to}>
+                {label}
+              </Link>
+            </li>
+          )
+        }}
+      />
+    )
+  }
+
+  const showMenu = (menus) => {
+    var result = null;
+    if (menus.length > 0) {
+      result = menus.map((menu, index) => {
+        return (
+          <MenuLink
+            key={index}
+            label={menu.name}
+            to={menu.to}
+            activeOnlyWhenExact={menu.exact}
+          />
+        )
+      })
+    }
+    return result;
+  }
   return (
     <div className="navbar navbar-default">
       <a className="navbar-brand">Call API</a>
       <ul className="nav navbar-nav">
-        <li>
-          <a>Trang chủ</a>
-        </li>
-        <li>
-          <a>Quản lí sản phẩm</a>
-        </li>
+        {showMenu(menus)}
       </ul>
     </div>
   );
