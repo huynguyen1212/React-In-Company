8664b0c6d8b1736438100a65b9a1544022404972
huynguyen1212 <huyro1212000@gmail.com>
2021-02-22 10:53:47 +0700

    lesson17-fragments

---
+++ lesson17-fragments/src/App.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>
+        Lập trình ReactJS
+      </h1>     
+      <h1>
+        Lập trình Angular
+      </h1>
+      <h1>
+        Lập trình Less
+      </h1>
+    </Fragment>
+  );
+}
+
+export default App;
